d78d211474dc1cb3a719101d8485498da7c6b234
Scott Metzel <scottmetzel@microsoft.com>
2025-01-13 21:04:29 -0800

    Moved Bearer Token function up, in case it needs to be used earlier, later.
    Changed discovery function to use a switch to match rest of code approach.
    
    Attempted to remove as much duplicate code as possible.

--- Azure_Arc/Enable-WindowsServerManagementByAzureArc.ps1
+++ Azure_Arc/Enable-WindowsServerManagementByAzureArc.ps1
@@ -34,6 +34,34 @@ else {
     throw
 }
 
+function CreateBearerTokenHeaderTable {
+    [PsCmdletbinding()]
+    [OutputType([System.Collections.Hashtable])]
+    param ()
+    [System.String]$ThisFunctionName = $MyInvocation.MyCommand
+    Write-Information -MessageData "Running: '$ThisFunctionName'."
+
+    Write-Information -MessageData 'Creating bearer token object.'
+    try {
+        $ErrorActionPreference = 'Stop'
+        $profile = [Microsoft.Azure.Commands.Common.Authentication.Abstractions.AzureRmProfileProvider]::Instance.Profile
+        $ProfileClient = [Microsoft.Azure.Commands.ResourceManager.Common.rmProfileClient]::new($profile)
+        $Token = $profileClient.AcquireAccessToken($GetAzContext.Subscription.TenantId)
+        [System.String]$BearerToken = [System.String]::Concat('Bearer ', $Token.AccessToken)
+        [System.Collections.Hashtable]$HeaderTable = @{
+            'Content-Type'  = 'application/json'
+            'Authorization' = $BearerToken
+        }
+
+        Write-Information -MessageData 'Outputting bearer token header table.'
+        $HeaderTable
+    }
+    catch {
+        $_
+        throw
+    }
+}
+
 function DiscoverMachines {
     [PsCmdletbinding()]
     [OutputType([System.Collections.ArrayList])]
@@ -67,55 +95,29 @@ function DiscoverMachines {
     try {
         $ErrorActionPreference = 'Stop'
 
-        if ($PSBoundParameters.ContainsKey('MachineNames')) {
-            Write-Information -MessageData "Getting specific Arc-enabled Servers in resource group: '$ResourceGroupName' in subscription: '$AzSubscriptionName'."
-            Get-AzResource -ResourceGroupName $ResourceGroupName -ResourceType 'Microsoft.HybridCompute/machines' | Where-Object -FilterScript { $_.Name -in $MachineNames } | Sort-Object -Property Name | ForEach-Object -Process {
-                $MachinesArray.Add($_) | Out-Null
+        switch ($PSCmdlet.ParameterSetName) {
+            'AzSubscription' {
+                Write-Information -MessageData "Getting specific Arc-enabled Servers in resource group: '$ResourceGroupName' in subscription: '$AzSubscriptionName'."
+                Get-AzResource -ResourceGroupName $ResourceGroupName -ResourceType 'Microsoft.HybridCompute/machines' | Where-Object -FilterScript { $_.Name -in $MachineNames } | Sort-Object -Property Name | ForEach-Object -Process {
+                    $MachinesArray.Add($_) | Out-Null
+                }
             }
-        }
-        elseif ($PSBoundParameters.ContainsKey('ResourceGroupName')) {
-            Write-Information -MessageData "Getting all Arc-enabled Servers in resource group: '$ResourceGroupName' in subscription: '$AzSubscriptionName'."
-            Get-AzResource -ResourceGroupName $ResourceGroupName -ResourceType 'Microsoft.HybridCompute/machines' | Sort-Object -Property Name | ForEach-Object -Process {
-                $MachinesArray.Add($_) | Out-Null
+            'ResourceGroup' {
+                Write-Information -MessageData "Getting all Arc-enabled Servers in resource group: '$ResourceGroupName' in subscription: '$AzSubscriptionName'."
+                Get-AzResource -ResourceGroupName $ResourceGroupName -ResourceType 'Microsoft.HybridCompute/machines' | Sort-Object -Property Name | ForEach-Object -Process {
+                    $MachinesArray.Add($_) | Out-Null
+                }
             }
-        }
-        else {
-            Write-Information -MessageData "Getting all Arc-enabled Servers in subscription: '$AzSubscriptionName'."
-            Get-AzResource -ResourceType 'Microsoft.HybridCompute/machines' | Sort-Object -Property Name | ForEach-Object -Process {
-                $MachinesArray.Add($_) | Out-Null
+            'Machines' {
+                Write-Information -MessageData "Getting all Arc-enabled Servers in subscription: '$AzSubscriptionName'."
+                Get-AzResource -ResourceType 'Microsoft.HybridCompute/machines' | Sort-Object -Property Name | ForEach-Object -Process {
+                    $MachinesArray.Add($_) | Out-Null
+                }
             }
         }
 
         Write-Information -MessageData 'Outputting result array.'
         $MachinesArray
-    }
-    catch {
-        $_
-        throw
-    }
-}
-
-function CreateBearerTokenHeaderTable {
-    [PsCmdletbinding()]
-    [OutputType([System.Collections.Hashtable])]
-    param ()
-    [System.String]$ThisFunctionName = $MyInvocation.MyCommand
-    Write-Information -MessageData "Running: '$ThisFunctionName'."
-
-    Write-Information -MessageData 'Creating bearer token object.'
-    try {
-        $ErrorActionPreference = 'Stop'
-        $profile = [Microsoft.Azure.Commands.Common.Authentication.Abstractions.AzureRmProfileProvider]::Instance.Profile
-        $ProfileClient = [Microsoft.Azure.Commands.ResourceManager.Common.rmProfileClient]::new($profile)
-        $Token = $profileClient.AcquireAccessToken($GetAzContext.Subscription.TenantId)
-        [System.String]$BearerToken = [System.String]::Concat('Bearer ', $Token.AccessToken)
-        [System.Collections.Hashtable]$HeaderTable = @{
-            'Content-Type'  = 'application/json'
-            'Authorization' = $BearerToken
-        }
-
-        Write-Information -MessageData 'Outputting bearer token header table.'
-        $HeaderTable
     }
     catch {
         $_
@@ -173,9 +175,12 @@ function AttestMachine {
     }
 }
 
+# Create an array list to collect responses for output at the end.
+[System.Collections.ArrayList]$ResponseArray = @()
+
 switch ($PSCmdlet.ParameterSetName) {
     'AzEnvironment' {
-        Write-Information -MessageData "Will attempt to attest to every Arc-enabled Server across all Azure subscriptions in Entra ID tenant: '$TenantID'."
+        Write-Information -MessageData "Will attempt to attest to all Arc-enabled Servers across all Azure subscriptions in Entra ID tenant: '$TenantID'."
         [System.Collections.ArrayList]$AzSubscriptions = @()
 
         Write-Information -MessageData 'Getting subscriptions'
@@ -190,26 +195,29 @@ switch ($PSCmdlet.ParameterSetName) {
         if (1 -le $AzSubscriptionCount) {
             Write-Information -MessageData "Found: '$AzSubscriptionCount' Azure subscriptions in tenant: '$TenantID'."
 
+            Write-Information -MessageData 'Getting Bearer token.'
             [System.Collections.Hashtable]$BearerTokenHeaderTable = CreateBearerTokenHeaderTable
 
             foreach ($AzSubscription in $AzSubscriptions) {
                 [System.String]$ThisAzSubscriptionName = $AzSubscription.Name
                 [System.String]$ThisAzSubscriptionID = $AzSubscription.Id
 
-                Write-Information -MessageData "Working on Arc-enabled Servers in subscription with name: '$ThisAzSubscriptionName' and id: '$ThisAzSubscriptionID'. Subscription: '$i' of: '$AzSubscriptionCount' subscriptions."
                 Write-Information -MessageData 'Setting context.'
                 Get-AzSubscription -SubscriptionId $ThisAzSubscriptionID | Set-AzContext
+
+                Write-Information -MessageData "Working on Arc-enabled Servers in subscription with name: '$ThisAzSubscriptionName' and ID: '$ThisAzSubscriptionID'. Subscription: '$i' of: '$AzSubscriptionCount' subscriptions."
                 [System.Collections.ArrayList]$MachinesArray = DiscoverMachines -AzSubscriptionID $ThisAzSubscriptionID
 
-                [System.Int32]$j = 1
                 [System.Int32]$MachinesArrayCount = $MachinesArray.Count
                 if (1 -le $MachinesArrayCount) {
                     Write-Information -MessageData "Found: '$MachinesArrayCount' Arc-enabled Servers."
 
                     Write-Information -MessageData 'Looping through machines...'
-                    [System.Collections.ArrayList]$ResponseArray = @()
-                    foreach ($Machine in $Machines) {
+                    [System.Int32]$j = 1
+                    foreach ($Machine in $MachinesArray) {
+                        [System.String]$MachineName = $Machine.Name
 
+                        Write-Information -MessageData "Working on server: '$MachineName'. Server: '$j' of: '$MachinesArrayCount' servers."
                         $Response = AttestMachine -Machine $Machine -BearerTokenHeaderTable $BearerTokenHeaderTable
 
                         $ResponseArray.Add($Response) | Out-Null
@@ -218,7 +226,7 @@ switch ($PSCmdlet.ParameterSetName) {
                     }
                 }
                 else {
-                    Write-Information -MessageData 'Found no Arc-enabled servers in current subscription.'
+                    Write-Information -MessageData 'Did not find any Arc-enabled servers in current subscription.'
                 }
 
                 $i++
@@ -229,68 +237,85 @@ switch ($PSCmdlet.ParameterSetName) {
         }
     }
     'AzSubscription' {
-        Write-Information -MessageData "Will attempt to attest to every Arc-enabled Server in Azure subscription: '$AzSubscriptionID'."
+        Write-Information -MessageData "Will attempt to attest to all Arc-enabled Servers in Azure subscription: '$AzSubscriptionID'."
+
+        if ($AzSubscriptionID -ne $GetAzContext.Subscription.Id) {
+            Write-Information -MessageData 'Setting context.'
+            Get-AzSubscription -SubscriptionId $AzSubscriptionID | Set-AzContext
+
+            Write-Information -MessageData 'Getting Azure Context again.'
+            $GetAzContext = Get-AzContext
+        }
+
+        Write-Information -MessageData 'Getting Bearer token.'
+        [System.Collections.Hashtable]$BearerTokenHeaderTable = CreateBearerTokenHeaderTable
+
+        [System.String]$ThisAzSubscriptionName = $GetAzContext.Subscription.Name
+        [System.String]$ThisAzSubscriptionID = $AzSubscriptionID
+
+        Write-Information -MessageData "Working on Arc-enabled Servers in subscription with name: '$ThisAzSubscriptionName' and ID: '$ThisAzSubscriptionID'."
+        [System.Collections.ArrayList]$MachinesArray = DiscoverMachines -AzSubscriptionID $ThisAzSubscriptionID
     }
     'ResourceGroup' {
-        Write-Information -MessageData "Will attempt to attest to every Arc-enabled Server in Azure resource group: '$ResourceGroupName'."
+        Write-Information -MessageData "Will attempt to attest to all Arc-enabled Servers in Azure resource group: '$ResourceGroupName'."
+
+        [System.String]$ThisAzSubscriptionName = $GetAzContext.Subscription.Name
+        [System.String]$ThisAzSubscriptionID = $GetAzContext.Subscription.Id
+
+        Write-Information -MessageData 'Getting Bearer token.'
+        [System.Collections.Hashtable]$BearerTokenHeaderTable = CreateBearerTokenHeaderTable
+
+        Write-Information -MessageData "Working on Arc-enabled Servers in subscription with name: '$ThisAzSubscriptionName', ID: '$ThisAzSubscriptionID', and in resource group: '$ResourceGroupName'."
+        [System.Collections.ArrayList]$MachinesArray = DiscoverMachines -ResourceGroupName $ResourceGroupName
     }
     'Machines' {
-        if ($MachineNames.Count -gt 1) {
+        if (1 -lt $MachineNames) {
             [System.String]$MachineNameString = $MachineNames -join ', '
         }
         else {
             [System.String]$MachineNameString = $MachineNames
         }
         Write-Information -MessageData "Will attempt to attest to these specific Arc-enabled Servers in the current Azure subscription: '$MachineNameString'."
+
+        [System.String]$ThisAzSubscriptionName = $GetAzContext.Subscription.Name
+        [System.String]$ThisAzSubscriptionID = $GetAzContext.Subscription.Id
+
+        Write-Information -MessageData 'Getting Bearer token.'
+        [System.Collections.Hashtable]$BearerTokenHeaderTable = CreateBearerTokenHeaderTable
+
+        Write-Information -MessageData "Working on Arc-enabled Servers in subscription with name: '$ThisAzSubscriptionName' and ID: '$ThisAzSubscriptionID', and in resource group: '$ResourceGroupName'."
+        [System.Collections.ArrayList]$MachinesArray = DiscoverMachines -MachineNames $MachineNames
     }
 }
 
-if ($PSBoundParameters.ContainsKey('AzSubscriptionID') -and ($AzSubscriptionID -ne $GetAzContext.Subscription.Id)) {
-    Write-Information -MessageData "Changing context to subscription: '$AzSubscriptionID'."
-    Get-AzSubscription -SubscriptionId $AzSubscriptionID | Set-AzContext | Out-Null
+# Separate switch block for attesting when working in a single subscription, resource group, or with individual machine names
+# The attestation code is identical once working at these scopes.
 
-    Write-Information -MessageData 'Getting Azure Context again.'
-    $GetAzContext = Get-AzContext
-}
+switch ($PSCmdlet.ParameterSetName) {
+    { $_ -in @('AzSubscription', 'ResourceGroup', 'Machines') } {
+        [System.Int32]$MachinesArrayCount = $MachinesArray.Count
+        if (1 -le $MachinesArrayCount) {
+            Write-Information -MessageData "Found: '$MachinesArrayCount' Arc-enabled Servers."
 
-[System.Int32]$i = 1
-[System.Int32]$MachinesArrayCount = $MachinesArray.Count
+            Write-Information -MessageData 'Looping through machines...'
+            [System.Int32]$j = 1
+            foreach ($Machine in $MachinesArray) {
+                [System.String]$MachineName = $Machine.Name
 
-if (0 -lt $MachinesArray.Count) {
-    Write-Information -MessageData "Found: '$MachinesArrayCount' Arc-enabled Servers."
+                Write-Information -MessageData "Working on server: '$MachineName'. Server: '$j' of: '$MachinesArrayCount' servers."
+                $Response = AttestMachine -Machine $Machine -BearerTokenHeaderTable $BearerTokenHeaderTable
 
+                $ResponseArray.Add($Response) | Out-Null
 
-
-    foreach ($Machine in $MachinesArray) {
-        [System.String]$MachineName = $Machine.Name
-        Write-Information -MessageData "Enabling Windows Server Management by Azure Arc on Server: '$MachineName'.  Server: '$i' of: '$MachinesArrayCount' servers."
-
-        try {
-            $ErrorActionPreference = 'Stop'
-
-            [System.Collections.Hashtable]$HeaderTable = CreateBearerTokenHeaderTable
-
-            $Response = AttestMachine -Machine $Machine
-            [PSCustomObject]$ResponseTable = @{
-                ServerNumber      = $i;
-                MachineName       = $MachineName;
-                ProvisioningState = $Response.Properties.provisioningState;
-                SoftwareAssurance = $Response.Properties.softwareAssurance;
+                $j++
             }
-            $ResponseArray.Add($ResponseTable) | Out-Null
-
         }
-        catch {
-            $_
-            throw
+        else {
+            Write-Information -MessageData 'Did not find any Arc-enabled servers in current subscription.'
         }
-
-        $i++
     }
 }
-else {
-    Write-Warning -Message "Didn't find any Arc-enabled Servers :( ."
-}
+
 Write-Information -MessageData 'Results: '
 $ResponseArray | Select-Object -Property 'ServerNumber', 'MachineName', 'ProvisioningState', 'SoftwareAssurance' | Format-Table -AutoSize
 
