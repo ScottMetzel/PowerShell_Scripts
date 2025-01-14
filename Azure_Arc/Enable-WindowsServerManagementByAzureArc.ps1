f770bdd49fb84db5b3e3abb1ff990df89e9e7ecd
Scott Metzel <scottmetzel@microsoft.com>
2025-01-14 09:38:28 -0800

    Adjusted comment formatting

--- Azure_Arc/Enable-WindowsServerManagementByAzureArc.ps1
+++ Azure_Arc/Enable-WindowsServerManagementByAzureArc.ps1
@@ -22,7 +22,7 @@
     Created by:   	Scott Metzel
     Organization: 	-
     Filename:     	Enable-WindowsServerManagementByAzureArc.ps1
-    Comments:       This script builds on Kevin Sullivan's original script, here:
+    Comments:     	This script builds on Kevin Sullivan's original script, here:
                     https://github.com/kevinsul/arc-ws-sa-enable/blob/main/arc-ws-sa-enable.ps1
     ===========================================================================
 
